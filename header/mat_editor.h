447b1aadbb69e992275b483a499d6d4f473837cc
asdhjklqe <dangcongnhatanh@gmail.com>
2024-11-06 20:38:30 +0700

    Update mat_editor.h

--- header/mat_editor.h
+++ header/mat_editor.h
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void delrow(int x[][100], int &n, int m, int y, int z){
+template <typename T>
+void delrow(T x[][100], int &n, int m, int y, int z){
     for (int i=y; i<=n-z; i++)
         for (int j=1; j<=m; j++)
             x[i][j] = x[i+z][j];
     n-= z;
 }
-void delcol(int x[][100], int n, int &m, int y, int z){
+template <typename T>
+void delcol(T x[][100], int n, int &m, int y, int z){
     for (int i=1; i<=n; i++)
         for (int j=y; j<=m-z; j++)
             x[i][j] = x[i][j+z];
     m-= z;
 }
-void addcol(int x[][100], int n, int &m, int y, int z){
+template <typename T>
+void addcol(T x[][100], int n, int &m, int y, int z){
     for (int i=1; i<=n; i++)
         for (int j=y; j<=m+z; j++){
             x[i][j+z] = x[i][j];
