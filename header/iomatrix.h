3953a4af8f0267169356f122614b24e69661aa9d
asdhjklqe <dangcongnhatanh@gmail.com>
2024-11-06 20:38:13 +0700

    Update iomatrix.h

--- header/iomatrix.h
+++ header/iomatrix.h
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void inpmat(int x[][100] ,int y ,int z){
+template <typename T>
+void inpmat(T x[][100] ,int y ,int z){
     for (int i=1; i<=y; i++)
         for (int j=1; j<=z; j++)
             cin >> x[i][j];
 }
-void outmat(int x[][100], int y, int z){
+template <typename T>
+void outmat(T x[][100], int y, int z){
     for (int i=1; i<=y; i++){
         for (int j=1; j<=z; j++)
             cout << x[i][j] << ' ';
         cout << '\n';
     }
 }
-int maxmat(int x[][100], int y , int z){
-    int maxx = x[1][1];
+template <typename T>
+T maxmat(T x[][100], int y , T z){
+    T maxx = x[1][1];
     for (int i=1; i<=y; i++)
         for (int j=1; j<=z; j++)
             maxx = fmax(maxx, x[i][j]);
     return maxx;
 }
-int minmat(int x[][100], int y, int z){
-    int minx = x[1][1];
+template <typename T>
+T minmat(T x[][100], int y, T z){
+    t  minx = x[1][1];
     for (int i=1; i<=y; i++)
         for (int j=1; j<=z; j++)
             minx = fmin(minx, x[i][j]);
