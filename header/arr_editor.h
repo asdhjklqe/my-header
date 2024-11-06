e6704ad708a4ec684f1fd307d75b7339be7d4929
asdhjklqe <dangcongnhatanh@gmail.com>
2024-11-06 20:35:53 +0700

    Update arr_editor.h

--- header/arr_editor.h
+++ header/arr_editor.h
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void del_element(int x[100], int &n, int y, int z){
+template <typename T>
+void del_element(T x[100], int &n, int y, int z){
     for (int i=y; i<=n-z; i++)
         x[i] = x[i+z];
     n-= z;
